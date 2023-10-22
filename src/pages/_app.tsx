62f756a587a7961e98077f3a2fd08939c196fe45
tzafff <christzaf99@gmail.com>
2023-10-22 17:28:46 +0300

    Add Auth Modals (Login,SignUp,ResetPassword) Integrate Recoil for State Manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Head>
+        <title>LeetClone</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content="Web Application that contains leetcode problems and video solution" />
+      </Head>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
