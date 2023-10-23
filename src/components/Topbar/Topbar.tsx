cd131933abc2702842bec5023fb486c6eccd72e8
tzafff <christzaf99@gmail.com>
2023-10-24 00:28:50 +0300

    Add TopBar Update on Auth and LogOut functionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,13 +1,22 @@
-import Link from "next/link";
 import React from "react";
+import { auth } from "@/firebase/firebase";
+import Link from "next/link";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+  const [user] = useAuthState(auth);
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
+    <>
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
-        className={`flex w-full items-center justify-between max-w[1200px] mx-auto`}
+        className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}
       >
         <Link href="/" className="h-[22px] flex-1">
           <img src="/logo-full.png" alt="Logo" height={100} width={100} />
@@ -24,14 +33,37 @@ const Topbar: React.FC<TopbarProps> = () => {
               Premium
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {!user && (
+            <Link href="/auth" onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}>
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          )}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <img
+                src="/avatar.png"
+                alt="Avatar"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
+          { user && <Logout />}
         </div>
       </div>
     </nav>
+    </>
+    
   );
 };
 export default Topbar;
