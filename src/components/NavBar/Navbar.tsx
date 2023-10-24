50fde5a66030d7076e1971eaa77a50a5bf196cb0
tzafff <christzaf99@gmail.com>
2023-10-25 01:21:32 +0300

    Update TopBar for problem page,add Timer Component

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { authModalState } from "@/atoms/authModalAtom";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 import { useSetRecoilState } from "recoil";
 
 type NavbarProps = {};
@@ -14,7 +15,8 @@ const Navbar: React.FC<NavbarProps> = () => {
 	return (
 		<div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
 		<Link href="/" className="flex items-center justify-center h-20">
-			<img src="/logo.png" alt="LeetClone" height={200} width={200} />
+
+			<Image src="/logo.png" alt="LeetClone" height={200} width={200} />
 		</Link>
 		<div className="flex items-center">
 			<button
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -5,65 +5,101 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import Logout from "../Buttons/Logout";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import Image from "next/image";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsList } from "react-icons/bs";
+import Timer from "../Timer/Timer";
 
-type TopbarProps = {};
+type TopbarProps = {
+  problemPage?: boolean;
+};
 
-const Topbar: React.FC<TopbarProps> = () => {
+const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
   const [user] = useAuthState(auth);
   const setAuthModalState = useSetRecoilState(authModalState);
 
   return (
     <>
-    <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
-      <div
-        className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}
-      >
-        <Link href="/" className="h-[22px] flex-1">
-          <img src="/logo-full.png" alt="Logo" height={100} width={100} />
-        </Link>
+      <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
+        <div
+          className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" :""}`}
+        >
+          <Link href="/" className="h-[22px] flex-1">
+            <Image src="/logo-full.png" alt="Logo" height={100} width={100} />
+          </Link>
 
-        <div className="flex items-center space-x-4 flex-1 justify-end">
-          <div>
-            <a
-              href="https://www.buymeacoffee.com/burakorkmezz"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2"
-            >
-              Premium
-            </a>
-          </div>
-          {!user && (
-            <Link href="/auth" onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}>
-              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-                Sign In
-              </button>
-            </Link>
-          )}
-          {user && (
-            <div className="cursor-pointer group relative">
-              <img
-                src="/avatar.png"
-                alt="Avatar"
-                width={30}
-                height={30}
-                className="rounded-full"
-              />
-              <div
-                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+          {[
+            problemPage && (
+              <div className="flex items-center gap-4 flex-1 justify-center">
+                <div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+                  <FaChevronLeft />
+                </div>
+                <Link
+                  href={"/"}
+                  className="flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer"
+                >
+                  <div>
+                    <BsList />
+                  </div>
+                  <p>Problem list</p>
+                </Link>
+                <div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+                  <FaChevronRight />
+                </div>
+              </div>
+            ),
+          ]}
+          <div className="flex items-center space-x-4 flex-1 justify-end">
+            <div>
+              <a
+                href="https://www.buymeacoffee.com/burakorkmezz"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2"
+              >
+                Premium
+              </a>
+            </div>
+            {!user && (
+              <Link
+                href="/auth"
+                onClick={() =>
+                  setAuthModalState((prev) => ({
+                    ...prev,
+                    isOpen: true,
+                    type: "login",
+                  }))
+                }
+              >
+                <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                  Sign In
+                </button>
+              </Link>
+            )}
+            {problemPage && <Timer />}
+            {user && (
+              <div className="cursor-pointer group relative">
+                <img
+                  src="/avatar.png"
+                  alt="Avatar"
+                  width={30}
+                  height={30}
+                  className="rounded-full"
+                />
+                <div
+                  className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
 								z-40 group-hover:scale-100 scale-0 
 								transition-all duration-300 ease-in-out"
-              >
-                <p className="text-sm">{user.email}</p>
+                >
+                  <p className="text-sm">{user.email}</p>
+                </div>
               </div>
-            </div>
-          )}
-          { user && <Logout />}
+            )}
+            {user && <Logout />}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
-    
   );
 };
 export default Topbar;
